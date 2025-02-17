a660f810241f5e07b0fe270564f457c54aa13fad
Joaquin Soto <joaquinsotorizzi@gmail.com>
2025-02-17 19:57:56 +0100

    adding unit test for Quote model

--- src/api/tests.py
+++ src/api/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from .models import Quote
 # Create your tests here.
+
+
+class QuoteTestCase(TestCase):
+    def setUp(self):
+        self.test_quote = Quote.objects.create(content="Test Quote", author="Test Author")
+
+    def test_quote_was_created(self):
+        self.assertTrue(self.test_quote.pk)
+
+    def test_quote_content(self):
+        self.assertEqual(self.test_quote.content, "Test Quote")
+
+    def test_quote_author(self):
+        self.assertEqual(self.test_quote.author, "Test Author")
